b440e978bf2d946eb4a3c45278042a7250b67ee9
Ray Hamel <rayghamel@gmail.com>
2015-05-02 22:50:12 -0400

    exercise 1.11

--- ex1_11.cc
+++ ex1_11.cc
@@ -8,10 +8,6 @@ int main()
 	int val1 = 0, val2 = 0;
 	std::cout << "Enter two integers:" << std::endl;
 	std::cin >> val1 >> val2;
-	/*
-	* Obviously, this is a silly way to do it, but I'm using only while loops
-	* because that's what's being covered in this section.
-	*/
 	while (val1 < val2) {
 		std::cout << val1 << std::endl;
 		++val1;

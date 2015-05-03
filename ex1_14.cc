1f4740b536e10a025c300954b7f8a5f728cc0775
Ray Hamel <rayghamel@gmail.com>
2015-05-02 23:32:55 -0400

    exercise 1.14

---
+++ ex1_14.cc
@@ -0,0 +1,14 @@
+/*
+* Compare and contrast the loops that used a "for" with those using a while. Are
+* there advantages or disadvantages to using either form?
+*/
+
+/*
+* The main advantage of the "for" loop over the "while" loop is that it allows
+* for fewer lines of code in many circumstances. Some disadvantages include that
+* the English semantic meaning of "for" deemphasizes the conditional aspects of
+* a loop as compared to "while", that all 3 statements of a "for" loop are not
+* always necessary, as with ex1_13_3, which omits the init-statement, and that
+* sometimes a programmer might want to create a loop with more than one or
+* variable exit conditions or expressions.
+*/

d8a070ef24fe8600c5edc07972ab6ffecdcda5eb
Ray Hamel <rayghamel@gmail.com>
2015-05-03 02:50:36 -0400

    exercise 1.25

---
+++ ex1_25.cc
@@ -0,0 +1,26 @@
+/*
+* Using the Sales_item.h header from the Web site, compile and execute the
+* bookstore program presented in this section.
+*/
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+	Sales_item total;
+	if (std::cin >> total) {
+		Sales_item trans;
+		while (std::cin >> trans) {
+			if (total.isbn() == trans.isbn())
+				total += trans;
+			else {
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
+	} else {
+		std::cerr << "No data?!" << std::endl;
+		return -1;
+	}
+	return 0;
+}

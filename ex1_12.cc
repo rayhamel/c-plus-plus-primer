69994cdd5e24cca61909940d9c55e3f304c26265
Ray Hamel <rayghamel@gmail.com>
2015-05-02 22:54:29 -0400

    exercise 1.12

---
+++ ex1_12.cc
@@ -0,0 +1,9 @@
+/*
+* What does the following "for" loop do? What is the final value of "sum"?
+*
+* int sum = 0;
+* for (int i = -100; i <= 100; ++1)
+*     sum += i;
+*/
+
+// It adds the integers in the range -100 to 100. The final value of "sum" is 0.

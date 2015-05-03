20b469792d1abf879e982408eb828219a1acdc48
Ray Hamel <rayghamel@gmail.com>
2015-05-03 01:10:45 -0400

    exercise 1.19

---
+++ ex1_18.cc
@@ -0,0 +1,39 @@
+/*
+* Compile and run the program from this section, giving it only equal values as
+* input. Run it again, giving it values in which no number is repeated.
+*/
+
+/*
+* When given all equal values:
+*
+* 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+* 1 occurs 14 times
+*
+* When given no repeats:
+*
+* 1 2 3 4
+* 1 occurs 1 times
+* 2 occurs 1 times
+* 3 occurs 1 times
+* 4 occurs 1 times
+*/
+#include <iostream>
+int main()
+{
+	int currVal = 0, val = 0;
+	if (std::cin >> currVal) {
+		int cnt = 1;
+		while (std::cin >> val) {
+			if (val == currVal)
+				++cnt;
+			else {
+				std::cout << currVal << " occurs " << cnt
+				          << " times" << std::endl;
+				currVal = val;
+				cnt = 1;
+			}
+		}
+		std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+	}
+	return 0;
+}

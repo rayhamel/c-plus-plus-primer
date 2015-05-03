8e117fc0929f728dabf5e301c388b52ad5a2d51e
Ray Hamel <rayghamel@gmail.com>
2015-05-03 00:59:48 -0400

    exercise 1.17

---
+++ ex1_17.cc
@@ -0,0 +1,10 @@
+/*
+* What happens in the program presented in this section if the input values are
+* all equal? What if there are no duplicated values?
+*/
+
+/*
+* If the input values are all equal, the program will write
+* "<n> appears <x> times" to the console only once. If there are no duplicated
+* values, then "<n> appears 1 times" would be printed for every value.
+*/
